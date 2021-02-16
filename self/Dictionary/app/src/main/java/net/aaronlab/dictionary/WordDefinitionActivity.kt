d28f19e8bee2585c5e70e2678d433bdd8552bfa0
Aaron Lee <aaronlab.net@gmail.com>
2021-02-16 16:10:21 +0900

    init view layouts

---
+++ self/Dictionary/app/src/main/java/net/aaronlab/dictionary/WordDefinitionActivity.kt
@@ -0,0 +1,11 @@
+package net.aaronlab.dictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WordDefinitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_definition)
+    }
+}
