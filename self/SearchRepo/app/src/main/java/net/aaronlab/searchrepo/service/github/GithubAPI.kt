ea650ef57c67f28abb090108034efa778793d24d
Aaron Lee <aaronlab.net@gmail.com>
2021-02-17 11:04:26 +0900

    add api interface

--- self/SearchRepo/app/src/main/java/net/aaronlab/searchrepo/service/github/GithubAPI.kt
+++ self/SearchRepo/app/src/main/java/net/aaronlab/searchrepo/service/github/GithubAPI.kt
@@ -1,4 +1,15 @@
 package net.aaronlab.searchrepo.service.github
 
+import net.aaronlab.searchrepo.models.GithubRepositoriesModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface GithubAPI {
+
+    @GET("search/repositories")
+    suspend fun getRepositories(
+        @Query("q") query: String
+    ): Response<GithubRepositoriesModel>
+
 }
