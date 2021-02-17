2a1fefc09ba4ca3a465774872c48f9442997896d
Aaron Lee <aaronlab.net@gmail.com>
2021-02-17 15:21:22 +0900

    edit api

--- self/MyNews/app/src/main/java/net/aaronlab/mynews/core/service/api/NewsAPI.kt
+++ self/MyNews/app/src/main/java/net/aaronlab/mynews/core/service/api/NewsAPI.kt
@@ -7,10 +7,26 @@ import retrofit2.http.Query
 
 interface NewsAPI {
 
+    /*
+    탑 헤드라인 뉴스 목록
+     */
     @GET("v2/top-headlines")
     suspend fun getTopHeadLines(
+        @Query("country") country: String = "us",
         @Query("pageSize") pageSize: Int,
-        @Query("page") page: Int
+        @Query("page") page: Int,
+        @Query("apiKey") apiKey: String = APIKey.KEY
+    ): Response<NewsResponse>
+
+    /*
+    전체 뉴스에서 검색
+     */
+    @GET("v2/everything")
+    suspend fun getSearchedNews(
+        @Query("q") query: String,
+        @Query("pageSize") pageSize: Int,
+        @Query("page") page: Int,
+        @Query("apiKey") apiKey: String = APIKey.KEY
     ): Response<NewsResponse>
 
 }
