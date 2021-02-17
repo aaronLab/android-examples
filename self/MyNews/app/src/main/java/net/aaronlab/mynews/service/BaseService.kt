a3735808029b2b8be99a79efc6c3d1d54b92cdde
Aaron Lee <aaronlab.net@gmail.com>
2021-02-17 15:11:07 +0900

    add base service

---
+++ self/MyNews/app/src/main/java/net/aaronlab/mynews/service/BaseService.kt
@@ -0,0 +1,15 @@
+package net.aaronlab.mynews.service
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class BaseService {
+
+    fun getClient(baseUrl: String): Retrofit? = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .client(OkHttpClient())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+}
