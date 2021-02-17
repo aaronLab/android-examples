cde39e49c500c3910808c6a9cba8c1c519368547
Aaron Lee <aaronlab.net@gmail.com>
2021-02-17 11:20:48 +0900

    add factory

---
+++ self/SearchRepo/app/src/main/java/net/aaronlab/searchrepo/viewmodels/MainViewModelFactory.kt
@@ -0,0 +1,17 @@
+package net.aaronlab.searchrepo.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import net.aaronlab.searchrepo.repositories.GithubRepository
+
+/*
+생성자에 파라미터가 있는 ViewModel 초기화를 위한 Factory
+ */
+class MainViewModelFactory(private val githubRepository: GithubRepository) :
+    ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(GithubRepository::class.java).newInstance(githubRepository)
+    }
+
+}
