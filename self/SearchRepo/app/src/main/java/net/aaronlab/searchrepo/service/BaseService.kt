2621b6cb1e9388060bbb6f4b88612fb6fd7648fe
Aaron Lee <aaronlab.net@gmail.com>
2021-02-17 11:07:14 +0900

    add comments

--- self/SearchRepo/app/src/main/java/net/aaronlab/searchrepo/service/BaseService.kt
+++ self/SearchRepo/app/src/main/java/net/aaronlab/searchrepo/service/BaseService.kt
@@ -4,6 +4,9 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+/*
+기본 서비스
+ */
 class BaseService {
 
     fun getClient(baseURL: String): Retrofit? = Retrofit.Builder()
