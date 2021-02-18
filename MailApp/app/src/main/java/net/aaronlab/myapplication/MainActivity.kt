27663bad1f5c36a313b00c5c7cdc9f2c654ebdab
Aaron Lee <aaronlab.net@gmail.com>
2021-02-19 00:20:36 +0900

    email

--- MailApp/app/src/main/java/net/aaronlab/myapplication/MainActivity.kt
+++ MailApp/app/src/main/java/net/aaronlab/myapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package net.aaronlab.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_send.setOnClickListener {
+            Log.d("!!!!!", edit_text_title.text.toString())
+        }
     }
 }
