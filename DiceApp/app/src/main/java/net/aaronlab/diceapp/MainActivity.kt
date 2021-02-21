b48cb5af3c8ce7d6fb618adfbe1efa028f3964a2
Aaron Lee <aaronlab.net@gmail.com>
2021-02-21 21:10:02 +0900

    findViewById

--- DiceApp/app/src/main/java/net/aaronlab/diceapp/MainActivity.kt
+++ DiceApp/app/src/main/java/net/aaronlab/diceapp/MainActivity.kt
@@ -2,11 +2,41 @@ package net.aaronlab.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupButton()
+    }
+
+    /*
+    Setup Roll Button
+     */
+    private fun setupButton() {
+        val rollButton = findViewById<Button>(R.id.btn_roll_dice)
+
+        rollButton.setOnClickListener {
+
+            rollDice()
+
+        }
+    }
+
+    /*
+    Roll Dice
+     */
+    private fun rollDice() {
+        // Pick a Random Num
+        val randomNumber = (1..6).random()
+
+        // Change Text
+        val randomNumberTxt = findViewById<TextView>(R.id.txt_random_number)
+        randomNumberTxt.text = randomNumber.toString()
     }
 
 }
